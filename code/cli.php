feb6c9947c534b22ffc20f815de2d763cba0102d
Kelly Kiernan <kelly@kellykiernan.com>
2016-05-11 17:00:06 -0400

    added folder for code samples

---
+++ code/cli.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|---------------------------------------------------------------------
+| CLI
+|---------------------------------------------------------------------
+|
+| When running on the command line, the $_SERVER global will contain
+| any command line arguments passed to the script. Check out some
+| more cool stuff below.
+|
+*/
+
+// Server and execution environment information is stored in $_SERVER.
+print_r($_SERVER);
+
+// 'argv' is an array of arguments passed to the script.
+print_r($_SERVER['argv']);
+
+// The first argument is always the script name.
+echo $_SERVER['argv'][0] . "\n";
+
+// $argv is a predefined variable that you can also use.
+print_r($argv);
