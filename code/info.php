75be5712bea786461329d7343770895836b0a67a
Kelly Kiernan <kelly@kellykiernan.com>
2016-05-25 21:43:32 -0400

    changed newline char for PHP_EOL constant

--- code/info.php
+++ code/info.php
@@ -19,3 +19,18 @@ print_r(get_loaded_extensions());
 
 // Prints information about the OS.
 echo php_uname();
+
+// Determines if a specific extension is loaded.
+var_dump(extension_loaded('mbstring'));
+
+// Gets the name of the owner of the current script.
+echo get_current_user() . PHP_EOL;
+
+// Prints an array of the function names in a module.
+print_r(get_extension_funcs('mbstring'));
+
+// Prints an array of constants.
+print_r(get_defined_constants());
+
+// Prints the path to the loaded ini file.
+echo php_ini_loaded_file();
