901328e06a66d56101fceb7e73b18db9b5170720
Kelly Kiernan <kelly@kellykiernan.com>
2016-05-26 23:27:42 -0400

    added createFromFormat example

--- code/datetime.php
+++ code/datetime.php
@@ -30,3 +30,15 @@ echo $tomorrow->format(DATETIME::RSS) . PHP_EOL;
 echo $tomorrow->format(DATETIME::RFC822) . PHP_EOL;
 echo $tomorrow->format(DATETIME::COOKIE) . PHP_EOL;
 echo $tomorrow->format(DATETIME::ATOM) . PHP_EOL;
+
+// Lets say we know our crazy client gave us a bunch of dates
+// in a yy/mm/dd format. Well, this is ambiguous for PHP.
+// How does it know which is the month, day, or year?
+$ambigousDate = '10/11/12';
+
+// The DateTime class can handle these kinds of
+// formats with the createFromFormat() method.
+$date = DateTime::createFromFormat('y/m/d', $ambigousDate);
+
+// Prints November 12, 2010
+echo $date->format('F j, Y');
